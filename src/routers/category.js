7ee5a1060d4b8ffe87647da654fab79529a34af6
freddieswift <freddie9117@live.co.uk>
2022-04-22 16:39:04 +0100

    Update year with created categories

--- src/routers/category.js
+++ src/routers/category.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Category = require('../models/category')
+const Year = require('../models/year')
 const auth = require('../middleware/auth')
 
 const router = new express.Router()
@@ -7,16 +8,38 @@ const router = new express.Router()
 //create category
 router.post('/category', auth, async (req, res) => {
 
+    if (req.body instanceof Array == false) {
+        return res.status(400).send()
+    }
+
     const categoriesArray = req.body
     const accountId = req.account._id
 
-    for (const category of categoriesArray) {
+    for (const category of req.body) {
         category.accountId = accountId
     }
 
     try {
-        const result = await Category.insertMany(categoriesArray)
-        res.status(201).send(result)
+        const categoryResult = await Category.insertMany(categoriesArray)
+
+        // if year is provided as a parameter
+        // update the year with the categories created
+        if (req.query.year) {
+
+            const yearResult = await Year.findOne({ _id: req.query.year })
+
+            if (!yearResult) {
+                return res.status(404).send({ error: "Unable to find year" })
+            }
+
+            for (const category of categoryResult) {
+                yearResult.categories.push(category._id)
+            }
+
+            await yearResult.save()
+        }
+
+        res.status(201).send(categoryResult)
     }
     catch (error) {
         res.status(500).send(error)

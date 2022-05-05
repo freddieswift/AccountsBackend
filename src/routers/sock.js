cf4a8c86daff11186322ad3cd91caed5d45ad5ba
freddieswift <freddie9117@live.co.uk>
2022-05-05 11:09:36 +0100

    Get sock routes

--- src/routers/sock.js
+++ src/routers/sock.js
@@ -9,7 +9,6 @@ const router = new express.Router()
 router.post('/sock', auth, async (req, res, next) => {
     const sock = new Sock(req.body)
     sock.accountId = req.account._id
-    console.log(sock)
     try {
         await sock.save()
         res.status(201).send(sock)
@@ -18,7 +17,45 @@ router.post('/sock', auth, async (req, res, next) => {
         next(error)
     }
 })
+
 //get sock by id
+router.get('/sock/:id', auth, async (req, res, next) => {
+    const accountId = req.account._id
+    const sockId = req.params.id
+
+    try {
+        const sock = await Sock.findOne({
+            _id: sockId,
+            accountId
+        })
+
+        if (!sock) {
+            return next(generateCustomError("No sock found", 404))
+        }
+
+        console.log(sock)
+        res.send(sock)
+    }
+    catch (error) {
+        next(error)
+    }
+})
+// get all socks
+
+router.get('/sock', auth, async (req, res, next) => {
+    const accountId = req.account._id
+
+    try {
+        const socks = await Sock.find({
+            "accountId": accountId
+        })
+
+        res.send({ socks: socks })
+    }
+    catch (error) {
+        next(error)
+    }
+})
 //update sock by id
 //delete sock by id
 

7acea45ea743f61c3fcdf59ceebff895c555a956
freddieswift <freddie9117@live.co.uk>
2022-08-24 21:41:16 +0100

    print connection string

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 const connectionURL = process.env.DATABASE_URL
+console.log(connectionURL)
 
 mongoose.connect(connectionURL, {
     useNewURLParser: true
